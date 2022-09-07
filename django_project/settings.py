76ad3a08fdade2e95ea0a549afcf071b807a28ed
Mohamed Abdelmagid <mohamedabdelmagid@Mohameds-MacBook-Air.local>
2022-09-06 22:59:19 -0400

    add user authentication

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,13 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
+    "django.contrib.sites",  # new
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
+    'allauth',  # new
+    'allauth.account',  # new
+    'allauth.socialaccount',  # new
+    'dj_rest_auth',
+    "dj_rest_auth.registration",  # new
 ]
 
 REST_FRAMEWORK = {  # new
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [  # new
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",  # new
     ],
 }
 
@@ -80,10 +91,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request", 
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  
+
+SITE_ID = 1  
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
